ca4e2545abdbe60d09a381564ac103ed82b03598
Stillwtm <2454340659@qq.com>
2022-01-17 19:11:45 +0800

    add hw1_1

---
+++ hw1_1/cmake-build-debug/CMakeFiles/learning.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/snorlax/CLionProjects/learning/catch_apple.cpp" "/home/snorlax/CLionProjects/learning/cmake-build-debug/CMakeFiles/learning.dir/catch_apple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
